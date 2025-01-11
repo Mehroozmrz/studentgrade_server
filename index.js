b6d3a0d175a771eb96a5ad14655c28817b302fa3
erpproject12 <139426885+erpproject12@users.noreply.github.com>
2025-01-11 11:59:40 +0400

    fift commit

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const dbConnection= require('./db');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const dbConnection = require("./db");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 
-app.use(express.json());    
+app.use(express.json());
 
-app.use(cors({
+app.use(
+  cors({
     origin: "https://studentgrade-frontend.vercel.app",
     // credentials: true,
-
-}));
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
 
 dbConnection();
 
-
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/student',require('./Route/studentRoute'));
-app.use('/api/subject',require('./Route/subjectRoute'));
-
+app.use("/api/student", require("./Route/studentRoute"));
+app.use("/api/subject", require("./Route/subjectRoute"));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
